a64169c4f2ced4c0164e8651b84d2221e29fe28b
shukroh890 <105241468+shukroh890@users.noreply.github.com>
2022-06-25 17:38:50 +0100

    Add files via upload

---
+++ assign.js
@@ -0,0 +1,14 @@
+let Name = "Name: idowu shukurat opeyemi";
+console.log(Name);
+
+
+let Height = "Height: 128cm"
+console.log(Height)
+
+var Country = "Country: Nigeria";
+console.log(Country)
+
+
+
+
+alert(Name+ "\n" +Height + "\n"+ Country);
